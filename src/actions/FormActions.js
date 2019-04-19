956f7c2d41da222bfdc573ff1a87b6b995d8d853
Traveler59 <dmitry_alekseevich@mail.ru>
2019-04-19 14:44:59 +0300

    async/await

--- src/actions/FormActions.js
+++ src/actions/FormActions.js
@@ -1,18 +1,16 @@
 import { SEND_ADDRESS } from './types';
 
-export const sendAddress = (address, selectors, site) => dispatch => {
-	fetch('/getArticle', {
+export const sendAddress = (address, selectors, site) => async dispatch => {
+	const response = await fetch('/getArticle', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify({ address: address, selectors: selectors, site: site }),
-	})
-		.then(res => res.json())
-		.then(fileReady =>
-			dispatch({
-				type: SEND_ADDRESS,
-				payload: fileReady
-			})
-		);
+	});
+	const articleReady = await response.json();
+	dispatch({
+		type: SEND_ADDRESS,
+		payload: articleReady
+	});
 };
