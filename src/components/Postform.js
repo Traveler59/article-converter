106ae5cc100f8fa339177d8df1ac253bac1c6357
Traveler59 <dmitry_alekseevich@mail.ru>
2019-07-02 09:06:46 +0300

    -//-

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -52,16 +52,16 @@ export default class PostForm extends Component {
 
 	render() {
 		const fileReady = this.props.fileReady;
-		const dosInput = '_'.repeat(67);
+		const dosInput = '_'.repeat(58);
 		const site = this.getSiteNameFromKnown(this.state.address);
 		return (
 			<Col id='main' lg={{ span: 6, offset: 3 }} md={12}>
 				<h1>Article Converter</h1>
 				<br />
 				<p>Save any webpage in .html format and exclude page elements that you don't need. Usefull for e-readers and offline desktop readers.</p>
-				<br /><br />
+				<br />
 				<Form onSubmit={this.onSubmit}>
-					<p>Full address of page that will be converted.</p>
+					<p>Full address of the page that will be converted.</p>
 					<Form.Group>
 						<Form.Label>Address: </Form.Label>
 						<Form.Control type='address' placeholder={dosInput} onChange={this.onChangeAddress} value={this.state.address} />
@@ -69,8 +69,8 @@ export default class PostForm extends Component {
 					{site
 						? <p className='highlighted-text'>Site was recognized, adding selectors not needed.</p>
 						: <div>
-							<br /><br />
-							<p>Set list of selectors, that will be removed, ex. '.footer' etc. They should be divided by end of line</p>
+							<br />
+							<p>Set list of selectors, that will be removed e.g. '.footer' etc. They should be divided by end of line.</p>
 							<p>Used selectors will be saved in cache.</p>
 							<Form.Group>
 								<Form.Label>Selectors: </Form.Label>
